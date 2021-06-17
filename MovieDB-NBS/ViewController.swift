0002c9a0d5d2e5ff327972dd45aa5c01afa86921
Ahmad Zaky W <azakyw@gmail.com>
2021-06-17 16:32:07 +0700

    Initial Commit

---
+++ MovieDB-NBS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB-NBS
+//
+//  Created by Ahmad Zaky on 17/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
